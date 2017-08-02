e198ab86a9c00189a7053b1a4ab903150b23ed28
Syed Hasnain <syedhasnain_bsse0035@live.com>
2017-08-02 19:34:58 +0500

    Add Changes

---
+++ Trolley/TolleyApp/TolleyApp/FruitCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCollectionViewCell.swift
+//  TolleyApp
+//
+//  Created by Rashdan Natiq on 02/08/2017.
+//  Copyright © 2017 Devclan. All rights reserved.
+//
+
+import UIKit
+
+class FruitCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Trolley/TolleyApp/TolleyApp/VegatableCollectionViewCell.swift
+++ Trolley/TolleyApp/TolleyApp/VegatableCollectionViewCell.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class VegatableCollectionViewCell: UICollectionViewCell {
-
+    var count = 0
+    @IBOutlet weak var countLable: UILabel!
+    @IBOutlet weak var hideView: UIView!
     @IBOutlet weak var vegImge: UIImageView!
     @IBOutlet weak var vegQantityLabel: UILabel!
     @IBOutlet weak var detailVegLabel: UILabel!
@@ -19,6 +21,23 @@ class VegatableCollectionViewCell: UICollectionViewCell {
         // Initialization code
     }
     @IBAction func didTapAddButton(_ sender: Any) {
+        hideView.alpha = 0.7
+        count = count + 1
+        countLable.text = "\(count)"
+    }
+    @IBAction func didTapHideButton(_ sender: UIButton) {
+        count = count - 1
+        if count == 0 {
+           hideView.alpha = 0
+        } else {
+            countLable.text = "\(count)"
+        }
+        
+        
+    }
+    @IBAction func didTapHideViewAddButton(_ sender: UIButton) {
+        count = count + 1
+        countLable.text = "\(count)"
     }
 
 }
